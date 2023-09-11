7b3c7996662751d95992c19d7f4ab0694e17cb92
TinyBila <bilatinym@gmail.com>
2023-09-11 10:47:09 -0700

    Printing a lost pf integers

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 0-print_list_integer.py
+
 def print_list_integer(_list=[]):
-    for i in _list:
-        print('{:d}'.format(i)
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
