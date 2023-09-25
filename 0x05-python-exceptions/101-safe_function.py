2e39a7263ae1913a87d83d8388ec58fef7d0e625
TinyBila <bilatinym@gmail.com>
2023-09-25 13:19:42 -0700

    updated

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except Exception:
-            result = b + a
-            break
-    return (result)
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return (result)
+    except Exception as i:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
