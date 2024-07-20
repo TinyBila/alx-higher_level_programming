bd817bfb56edb13c7bdb425013e9a39c7980c04c
TinyBila <bilatinym@gmail.com>
2024-07-20 10:46:48 -0700

    selected best score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
--- A  script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- A script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.
 SELECT score, name FROM second_table WHWRE score >= 10 ORDER BY score DESC;
 
